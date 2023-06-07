10bf366eb1b0a1e98bd9628af6da2925444e78c5
Martin B <martin.blazquez.19@gmail.com>
2023-06-07 22:53:52 +0200

    HomeFragment show the phrase from the database on Firebase. FINALLY!

--- app/src/main/java/com/tgf/ecoapp/ui/home/HomeViewModel.java
+++ app/src/main/java/com/tgf/ecoapp/ui/home/HomeViewModel.java
@@ -4,13 +4,32 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class HomeViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+
+        // Obtiene la referencia a Firestore
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        // Accede a tu documento
+        db.collection("FRASES").document("YkCRcGBxvrNdD1zhd2PC")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            // Asume que el campo que quieres mostrar se llama "miCampo"
+                            String data = document.getString("Frase");
+                            mText.setValue(data);
+                        }
+                    }
+                });
     }
 
     public LiveData<String> getText() {
