ec319d1e8c730ab497303044540aa0edda9d1ee1
Martin B <martin.blazquez.19@gmail.com>
2023-06-08 00:33:46 +0200

    I add code to change the phrase of database everyday. I needed to create the class 'HomeViewModelFactory' so that 'HomeViewModel' would not handle the information.

--- app/src/main/java/com/tgf/ecoapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/tgf/ecoapp/ui/home/HomeFragment.java
@@ -18,8 +18,10 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+                new ViewModelProvider(this, new HomeViewModelFactory(requireActivity().getApplication()))
+                        .get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
--- app/src/main/java/com/tgf/ecoapp/ui/home/HomeViewModel.java
+++ app/src/main/java/com/tgf/ecoapp/ui/home/HomeViewModel.java
@@ -1,33 +1,52 @@
 package com.tgf.ecoapp.ui.home;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class HomeViewModel extends ViewModel {
+public class HomeViewModel extends AndroidViewModel {
 
     private final MutableLiveData<String> mText;
+    private static final String PREFS_NAME = "MyPrefs";
+    private static final String FRASE_KEY = "FraseKey";
 
-    public HomeViewModel() {
+    public HomeViewModel(Application application) {
+        super(application);
         mText = new MutableLiveData<>();
 
-        // Obtiene la referencia a Firestore
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        // Accede a tu documento
-        db.collection("FRASES").document("YkCRcGBxvrNdD1zhd2PC")
+        // Obtén el número de la última frase que se mostró de SharedPreferences
+        SharedPreferences prefs = getApplication().getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        int lastFraseNumber = prefs.getInt(FRASE_KEY, 0);
+
+        // Calcula el número de la próxima frase
+        int nextFraseNumber = (lastFraseNumber % 40) + 1;
+
+        // Recupera el documento que tiene el "Día" igual al número calculado
+        db.collection("FRASES")
+                .whereEqualTo("Día", String.valueOf(nextFraseNumber))  // Asegúrate de que "Día" esté escrito exactamente como en tu base de datos
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        DocumentSnapshot document = task.getResult();
-                        if (document.exists()) {
-                            // Asume que el campo que quieres mostrar se llama "miCampo"
+                        for (DocumentSnapshot document : task.getResult()) {
                             String data = document.getString("Frase");
                             mText.setValue(data);
                         }
+
+                        // Almacena el número de la frase mostrada en SharedPreferences
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.putInt(FRASE_KEY, nextFraseNumber);
+                        editor.apply();
                     }
                 });
     }
