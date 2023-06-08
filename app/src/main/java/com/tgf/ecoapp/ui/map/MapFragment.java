d2f7a3e52ec6cecb03bb7a0a117052907d6ec1e8
Martin B <martin.blazquez.19@gmail.com>
2023-06-08 02:41:45 +0200

    Add one marker from Firebase. Change colour of marker, add zoom buttons

--- app/src/main/java/com/tgf/ecoapp/ui/map/MapFragment.java
+++ app/src/main/java/com/tgf/ecoapp/ui/map/MapFragment.java
@@ -11,13 +11,18 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.tgf.ecoapp.R;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FirebaseFirestore db;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,6 +34,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this); // Configurar el callback cuando el mapa esté listo
 
+        // Inicializar Firestore
+        db = FirebaseFirestore.getInstance();
+
         return root; // Devolver la vista raíz
     }
 
@@ -36,10 +44,42 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Agregar un marcador en Madrid
-        LatLng madrid = new LatLng(40.416775, -3.703790);
-        //mMap.addMarker(new MarkerOptions().position(madrid).title("Marker in Madrid")); // Agregar un marcador en Madrid, pero actualmente no es necesario
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(madrid, 15)); // Hacer zoom a Madrid con un nivel de zoom de 15
-    }
+        // Enable Zoom Controls
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setPadding(0, 0, 0, 100);  // Move the zoom controls up
 
+        LatLng madrid = new LatLng(40.416775, -3.703790);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(madrid, 15));
+
+        // Read data from Firestore and add markers
+        db.collection("ContenedoresLite")
+                .document("ENVASES")
+                .collection("CENTRO")
+                .document("167909")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            Double latitud = document.getDouble("LATITUD");
+                            Double longitud = document.getDouble("LONGITUD");
+                            String nombre = document.getString("Nombre");
+
+                            if (latitud != null && longitud != null && nombre != null) {
+                                LatLng containerLatLng = new LatLng(latitud, longitud);
+                                mMap.addMarker(new MarkerOptions()
+                                        .position(containerLatLng)
+                                        .title(nombre)
+                                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))); // Set marker color to yellow
+                            } else {
+                                // Log: One of the required fields is null
+                            }
+                        } else {
+                            // Log: No such document
+                        }
+                    } else {
+                        // Log: Error getting document
+                    }
+                });
+    }
 }
--- app/src/main/java/com/tgf/ecoapp/ui/map/MapsContainer.java
+++ app/src/main/java/com/tgf/ecoapp/ui/map/MapsContainer.java
@@ -1,8 +1,32 @@
 package com.tgf.ecoapp.ui.map;
-
 /**
  * Created by Martin B. on 8/6/23.
  * martin.blazquez.dam@gmail.com
  */
 public class MapsContainer {
+    private String nombre;
+    private double latitud;
+    private double longitud;
+
+    public MapsContainer() {
+        // Constructor vacío necesario para Firebase
+    }
+
+    public MapsContainer(String nombre, double latitud, double longitud) {
+        this.nombre = nombre;
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
 }
